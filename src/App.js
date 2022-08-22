157d9059174d5d693fe32ba5fde39ece79d20447
prithviya <geethaa14367@gmail.com>
2022-08-22 21:29:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  function handCallbackResponse(response)
+  {
+    console.log("Encoded JWT ID  tokent:"+response.credential);
+  }
+  
+  useEffect(()=>{
+  google.accounts.id.initalize({
+    client_id:"908073531216-aa77ui5tqhgau6kis4hkm3s5t6gb63j3.apps.googleusercontent.com",
+    callback: handCallbackResponse
+  });
+  google.account.id.renderButton(
+    document.getElementById("signInDiv"),
+    {
+      theme: "outline", size: "large" 
+    }
+  );
+  },[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="signInDiv"></div>
     </div>
   );
 }
